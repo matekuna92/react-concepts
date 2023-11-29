fe182b241eaba4f81c6eacc2d838e29b4ae05f65
matekuna92 <kunamate66@gmail.com>
2023-11-29 18:51:10 +0100

    isLoading state

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -8,6 +8,7 @@ const PostsList = ({ isPosting, onShowPosts, onCancel, onAddNewPost }) => {
     const [enteredDesc, setEnteredDesc] = useState("");
     const [enteredAuthor, setEnteredAuthor] = useState("");
     const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     const changeDescHandler = (event) => {
         setEnteredDesc(event.target.value);
@@ -43,42 +44,48 @@ const PostsList = ({ isPosting, onShowPosts, onCancel, onAddNewPost }) => {
 
 	useEffect(() => {
         async function fetchPosts() {
+            setIsLoading(true);
             const response = await fetch("http://localhost:8080/posts");
             const responseData = await response.json();
-			setPosts(responseData.posts);	// backend returns a response called "posts": res.json({ posts: storedPosts });
+            setPosts(responseData.posts); // backend returns a response called "posts": res.json({ posts: storedPosts });
+            setIsLoading(false);
         }
 
 		fetchPosts();
     }, []);
 
     return (
-		<>
-			{isPosting && (
-				<Modal onHideBackdrop={onShowPosts}>
-					<NewPost
-						onDescChange={changeDescHandler}
-						onAuthorChange={changeAuthorHandler}
-						onCancel={onShowPosts}
-						onFormSubmit={formSubmitHandler}
-					/>
-				</Modal>
-			)}
+        <>
+            {isPosting && (
+                <Modal onHideBackdrop={onShowPosts}>
+                    <NewPost
+                        onDescChange={changeDescHandler}
+                        onAuthorChange={changeAuthorHandler}
+                        onCancel={onShowPosts}
+                        onFormSubmit={formSubmitHandler}
+                    />
+                </Modal>
+            )}
 
-			{posts.length > 0 ? (
-				<ul className={classes.posts}>
-					{posts.map((post) => (
-						<Post
-							key={post.id}
-							author={post.author}
-							desc={post.desc}
-						/>
-					))}
-				</ul>
-			) : (
-				<p>There are no posts yet. Add the first one!</p>
-			)}
-		</>
-	);
+            {!isLoading && posts.length > 0 && (
+                <ul className={classes.posts}>
+                    {posts.map((post) => (
+                        <Post
+                            key={post.id}
+                            author={post.author}
+                            desc={post.desc}
+                        />
+                    ))}
+                </ul>
+            )}
+
+            {!isLoading && posts.length == 0 && (
+                <p>There are no posts yet. Add the first one!</p>
+            )}
+
+            {isLoading && <p>Loading posts...</p>}
+        </>
+    );
 };
 
 export default PostsList;
